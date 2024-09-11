8405413e9b084ba155178d12404afde2adb02e33
BoogieNaipe <marcelovldgomes@outlook.com>
2024-09-11 09:31:16 -0300

    Calculo simples do fibonacci

---
+++ src/main.rs
@@ -0,0 +1,25 @@
+use std::io;
+
+fn fib(n: i64) -> i64 {
+    if n == 0 {
+        return 0;
+    } else if n == 1 {
+        return 1;
+    } else {
+        return fib(n - 1) + fib(n - 2);
+    }
+}
+
+fn main() {
+    println!("Digite um número positivo: ");
+
+    let mut n = String::new();
+    io::stdin()
+        .read_line(&mut n)
+        .expect("Digite um número válido...");
+    let n: i64 = n.trim().parse()
+        .expect("Por favor, digite um número...");
+
+    let resultado = fib(n);
+    println!("O termo {} da sequência de Fibonacci é: {}\n", n, resultado);
+}
